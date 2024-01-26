597a2cfc470d4a646ed490a4195bef6bf5ed2896
leumij <cjimuel74@gmail.com>
2024-01-26 17:58:35 +0800

    pseudocode

---
+++ palindrome.py
@@ -0,0 +1,6 @@
+# Write a program to check if the given number is a palindrome number.
+# A palindrome number is a number that is the same after reverse. For example, 545, is the palindrome numbers
+
+# Pseudocode
+# Ask user for input
+# If else for original number and reserve number
