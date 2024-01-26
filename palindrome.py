884d1c7fd4afa9060798d1c85c9648152525a3de
leumij <cjimuel74@gmail.com>
2024-01-26 18:09:17 +0800

    if else

--- palindrome.py
+++ palindrome.py
@@ -3,4 +3,12 @@
 
 # Pseudocode
 # Ask user for input
+number = input("Enter a 3 digit number: ")
+print("Original number ",number)
+reverse_num = str(number[::-1])
+
 # If else for original number and reserve number
+if int(number) == int(reverse_num):
+    print("Yes. given number is palindrome number")
+else:
+    print("No. given number is not palindrome number")
